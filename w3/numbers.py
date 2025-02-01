12452818a90e86a8eb2120464f1d543c74f84bb3
uudx <155887302+uudx@users.noreply.github.com>
2025-02-01 13:33:59 +0500

    Create numbers.py

---
+++ w3/numbers.py
@@ -0,0 +1,15 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
